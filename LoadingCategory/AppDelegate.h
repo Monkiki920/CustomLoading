ebf69caf6d1bf52de2d017c5dc7e42de4e679e86
ricky <1049260505@qq.com>
2019-03-20 15:44:51 +0800

    Initial Commit

---
+++ LoadingCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoadingCategory
+//
+//  Created by Ricky on 2019/3/20.
+//  Copyright © 2019 Ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoadingCategory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoadingCategory
+//
+//  Created by Ricky on 2019/3/20.
+//  Copyright © 2019 Ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
